d5a99c0a09903f2168d6355f5bb528b57b8feb76
Yamen Shahin <yamenshahin@gmail.com>
2018-03-06 01:37:55 +0200

    Log User In and Out

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -30,4 +30,57 @@ class AuthController extends Controller
         User::create(['username' => $username, 'email' => $email, 'password' => Hash::make($password)]);
         return $this->login($request);
     }
+
+    /**
+     * API Login, on success return JWT Auth token
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        $rules = [
+            'email' => 'required|email',
+            'password' => 'required',
+        ];
+
+        $validator = Validator::make($credentials, $rules);
+        if($validator->fails()) {
+            return response()->json(['success'=> false, 'error'=> $validator->messages()]);
+        }
+
+        try {
+            // attempt to verify the credentials and create a token for the user
+            if (!$token = JWTAuth::attempt($credentials)) {
+                return response()->json(['success' => false, 'error' => 'We cant find an account with this credentials.'], 401);
+            }
+        } catch (JWTException $e) {
+            // something went wrong whilst attempting to encode the token
+            return response()->json(['success' => false, 'error' => 'Failed to login, please try again.'], 500);
+        }
+
+        // all good so return the token
+        return response()->json(['success' => true, 'data'=> [ 'token' => $token ]]);
+    }
+
+    /**
+     * Log out
+     * Invalidate the token, so user cannot use it anymore
+     * They have to relogin to get a new token
+     *
+     * @param Request $request
+     */
+    public function logout(Request $request) {
+        $this->validate($request, ['token' => 'required']);
+
+        try {
+            JWTAuth::invalidate($request->input('token'));
+            return response()->json(['success' => true, 'message'=> "You have successfully logged out."]);
+        } catch (JWTException $e) {
+            // something went wrong whilst attempting to encode the token
+            return response()->json(['success' => false, 'error' => 'Failed to logout, please try again.'], 500);
+        }
+    }
 }
