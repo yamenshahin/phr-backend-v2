fe2b2fe732f226ea7e5c0cdbe008af5542e8d12c
Yamen Shahin <yamenshahin@gmail.com>
2018-03-06 00:54:12 +0200

    Add JWT Provider and Facades

--- config/app.php
+++ config/app.php
@@ -160,6 +160,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * jwt-auth : JSON Web Token Authentication for Laravel & Lumen
+         */
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+
     ],
 
     /*
@@ -208,6 +213,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class, 
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
 
     ],
 
