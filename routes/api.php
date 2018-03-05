f70105a458e988061a7134ee21cf8fdc89783108
Yamen Shahin <yamenshahin@gmail.com>
2018-03-06 01:00:40 +0200

    Set Up Routes: Web Routes: password reset & API Routes: register, login, recover, logout (middleware jwt.auth)

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,14 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+/*
+|--------------------------------------------------------------------------
+| API Routes: register, login, recover, logout (middleware jwt.auth)
+|--------------------------------------------------------------------------
+*/
+Route::post('register', 'AuthController@register');
+Route::post('login', 'AuthController@login');
+Route::post('recover', 'AuthController@recover');
+Route::group(['middleware' => ['jwt.auth']], function() {
+    Route::get('logout', 'AuthController@logout');
+});
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+/*
+|--------------------------------------------------------------------------
+| Web Routes: password reset
+|--------------------------------------------------------------------------
+*/
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.request');
+Route::post('password/reset', 'Auth\ResetPasswordController@postReset')->name('password.reset');
