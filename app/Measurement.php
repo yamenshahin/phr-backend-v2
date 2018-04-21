c6dbc22f37bd4797d80a1afe962100daa2c4dcde
Yamen Shahin <yamenshahin@gmail.com>
2018-04-21 02:17:44 +0200

    API Routes: measurements. Select and arrange JSON keys

--- app/Measurement.php
+++ app/Measurement.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Measurement extends Model
 {
@@ -19,21 +20,23 @@ class Measurement extends Model
     public function measurement_meta() {
         return $this->hasMany(MeasurementMeta::class);
     }
+    /**
+     * For API measurements, measurements/{user_id}, measurements/{user_id}/{measurement_name} 
+     */
     public function scopeGetMeasurement($query, $user_id, $name = null) {
     	if ($name !== null) {
-    		$measurements = $query->where('user_id', $user_id)
+    		$measurements = $query->select(DB::raw('measurement_id, name, measurement_metas.key, measurement_metas.value, unit, note, date_taken, created_at, updated_at, unit_id'))->where('user_id', $user_id)
     			->where('name', $name)
     			->join('measurement_metas', 'measurements.id', '=', 'measurement_metas.measurement_id')
     			->join('units', 'measurement_metas.unit_id', '=', 'units.id');
     		
     	} else {
-    		$measurements = $query->where('user_id', $user_id)
+    		$measurements = $query->select(DB::raw('measurement_id, name, measurement_metas.key, measurement_metas.value, unit, note, date_taken, created_at, updated_at, unit_id'))->where('user_id', $user_id)
     			->join('measurement_metas', 'measurements.id', '=', 'measurement_metas.measurement_id')
     			->join('units', 'measurement_metas.unit_id', '=', 'units.id');
     	}
 
+    	
     	return $measurements;
     }
-
-    
 }
