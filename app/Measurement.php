ae14cb7a5156b59899f58e876e730bd88154c6b0
Yamen Shahin <yamenshahin@gmail.com>
2018-04-29 02:38:22 +0200

    Temp Measurement replace user_id with subuser_id + Temp FIX for UnitFactory

--- app/Measurement.php
+++ app/Measurement.php
@@ -20,17 +20,17 @@ class Measurement extends Model
         return $this->hasMany(MeasurementMeta::class);
     }
     /**
-     * For API measurements, measurements/{user_id}, measurements/{user_id}/{measurement_name} 
+     * For API measurements, measurements/{user_id}/{subuser_id}, measurements/{user_id}/{user_id}/{subuser_id}/{measurement_name} 
      */
-    public function scopeGetMeasurement($query, $user_id, $name = null) {
+    public function scopeGetMeasurement($query, $user_id, $subuser_id, $name = null) {
     	if ($name !== null) {
-    		$measurements = $query->select(DB::raw('measurement_id, name, measurement_metas.key, measurement_metas.value, unit, note, date_taken, created_at, updated_at, unit_id'))->where('user_id', $user_id)
+    		$measurements = $query->select(DB::raw('measurement_id, name, measurement_metas.key, measurement_metas.value, unit, note, date_taken, created_at, updated_at, unit_id'))->where('subuser_id', $subuser_id)
     			->where('name', $name)
     			->join('measurement_metas', 'measurements.id', '=', 'measurement_metas.measurement_id')
     			->join('units', 'measurement_metas.unit_id', '=', 'units.id');
     		
     	} else {
-    		$measurements = $query->select(DB::raw('measurement_id, name, measurement_metas.key, measurement_metas.value, unit, note, date_taken, created_at, updated_at, unit_id'))->where('user_id', $user_id)
+    		$measurements = $query->select(DB::raw('measurement_id, name, measurement_metas.key, measurement_metas.value, unit, note, date_taken, created_at, updated_at, unit_id'))->where('subuser_id', $subuser_id)
     			->join('measurement_metas', 'measurements.id', '=', 'measurement_metas.measurement_id')
     			->join('units', 'measurement_metas.unit_id', '=', 'units.id');
     	}
--- database/factories/UnitFactory.php
+++ database/factories/UnitFactory.php
@@ -1,23 +1,12 @@
 <?php
 
 use Faker\Generator as Faker;
-//FIXME
-$factory->define(App\Unit::class, function (Faker $faker) {
-	/*$uints = array('mg/dl',
-					'mmol/L',
-					'mmHg',
-					'beats per minute',
-					'ft',
-					'in',
-					'cm',
-					'm',
-					'liters/minute',
-					'liters',
-					'lbs',
-					'kg'
-				);*/
+
+//Define uints list FIX
+$uints = array('mg/dl','mmol/L','mmHg','beats per minute','ft','in','cm','m','liters/minute','liters','lbs','kg');
+$factory->define(App\Unit::class, function (Faker $faker) use ($uints){
 	return [
-		// 'unit' => $uints[$i];
+		'unit' => $uints[$id]
 	];
 });
 
