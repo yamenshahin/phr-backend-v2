d2b52923f5ceb15ced9b91353df9e3e580448786
Yamen Shahin <yamenshahin@gmail.com>
2018-07-04 03:41:08 +0200

    Set login error message to 'Email or password is incorrect'VR-227 Refactor Listing Search and Map Results;

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -29,7 +29,7 @@ class AuthController extends Controller
         $credentials = request(['email', 'password']);
 
         if (! $token = auth()->attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+            return response()->json(['error' => 'Email or password is incorrect!'], 401);
         }
 
         return $this->respondWithToken($token);
